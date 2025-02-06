ae8c5522779c92f458b5e815f47ffc970b556a33
diCugno <dialessandro@itis-molinari.eu>
2025-02-06 18:08:11 +0100

    aggiunto client server html

---
+++ public/index.js
@@ -0,0 +1,36 @@
+const inputFile = document.querySelector('#file');
+const button = document.querySelector("#button");
+const link = document.querySelector("#link");
+
+const render = async () => {
+    const list = await fetch("/filelist");
+    const data = await list.json();
+
+    const html = data
+        .map(e => `<li><a href="${e}">${e}</a></li>`)
+        .join("");
+
+    link.innerHTML = `<ul>${html}</ul>`; 
+};
+
+
+await render();
+
+const handleSubmit = async (event) => {
+    const formData = new FormData();
+    formData.append("file", inputFile.files[0]);
+    const body = formData;
+    const fetchOptions = {
+        method: 'post',
+        body: body
+    };
+    try {
+        const res = await fetch("/upload", fetchOptions);
+        const data = await res.json();
+        await render();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+button.onclick = handleSubmit;
